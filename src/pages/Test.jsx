3cf63f29a3ac896083680da68bf2bbd93b6a9129
Caleb Hodges <chodgesdev@gmail.com>
2023-09-21 11:04:07 -0500

    Remove backend test

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,32 +1,38 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+
+// const Test = () => {
+//   const [members, setMembers] = useState();
+
+//   const baseURL = `http://localhost:4004/api`;
+
+//   useEffect(() => {
+//     async function fetchMembers() {
+//       setMembers(null);
+//       const result = await axios.get(`${baseURL}/members`);
+//       if (!ignore) {
+//         setMembers(result);
+//       }
+//     }
+
+//     let ignore = false;
+//     fetchMembers();
+//     return () => {
+//       ignore = true;
+//     };
+//   }, [baseURL]);
+
+//   if (members) {
+//     console.log(members.data);
+//   }
+
+//   return <div>{members && members.data.map(member => <h1 key={member.id}>{member.name}</h1>)}</div>;
+// };
+
+// export default Test;
 
 const Test = () => {
-  const [members, setMembers] = useState();
-
-  const baseURL = `http://localhost:4004/api`;
-
-  useEffect(() => {
-    async function fetchMembers() {
-      setMembers(null);
-      const result = await axios.get(`${baseURL}/members`);
-      if (!ignore) {
-        setMembers(result);
-      }
-    }
-
-    let ignore = false;
-    fetchMembers();
-    return () => {
-      ignore = true;
-    };
-  }, [baseURL]);
-
-  if (members) {
-    console.log(members.data);
-  }
-
-  return <div>{members && members.data.map(member => <h1 key={member.id}>{member.name}</h1>)}</div>;
-};
+    return <h1>Now Showing the Test Page</h1>
+}
 
 export default Test;
