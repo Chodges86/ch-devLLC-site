6df587aa0611394dbc230559949fca8074195538
Caleb Hodges <chodgesdev@gmail.com>
2023-09-21 12:15:43 -0500

    testing digital ocean

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,38 +1,38 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-
-// const Test = () => {
-//   const [members, setMembers] = useState();
-
-//   const baseURL = `http://localhost:4004/api`;
-
-//   useEffect(() => {
-//     async function fetchMembers() {
-//       setMembers(null);
-//       const result = await axios.get(`${baseURL}/members`);
-//       if (!ignore) {
-//         setMembers(result);
-//       }
-//     }
-
-//     let ignore = false;
-//     fetchMembers();
-//     return () => {
-//       ignore = true;
-//     };
-//   }, [baseURL]);
-
-//   if (members) {
-//     console.log(members.data);
-//   }
-
-//   return <div>{members && members.data.map(member => <h1 key={member.id}>{member.name}</h1>)}</div>;
-// };
-
-// export default Test;
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Test = () => {
-    return <h1>Now Showing the Test Page</h1>
-}
+  const [members, setMembers] = useState();
+
+  const baseURL = `http://localhost:4004/api`;
+
+  useEffect(() => {
+    async function fetchMembers() {
+      setMembers(null);
+      const result = await axios.get(`${baseURL}/members`);
+      if (!ignore) {
+        setMembers(result);
+      }
+    }
+
+    let ignore = false;
+    fetchMembers();
+    return () => {
+      ignore = true;
+    };
+  }, [baseURL]);
+
+  if (members) {
+    console.log(members.data);
+  }
+
+  return <div>{members && members.data.map(member => <h1 key={member.id}>{member.name}</h1>)}</div>;
+};
 
 export default Test;
+
+// const Test = () => {
+//     return <h1>Now Showing the Test Page</h1>
+// }
+
+// export default Test;
