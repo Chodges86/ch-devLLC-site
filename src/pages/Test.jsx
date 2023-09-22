4077b98336cd599715fb6165d0dfef00dc383ed1
Caleb Hodges <chodgesdev@gmail.com>
2023-09-21 20:22:18 -0500

    testing DO droplet

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -4,13 +4,13 @@ import { useEffect, useState } from "react";
 const Test = () => {
   const [members, setMembers] = useState();
 
-  const baseURL = `https://lobster-app-oiavg.ondigitalocean.app/api`;
+  // const baseURL = `https://lobster-app-oiavg.ondigitalocean.app/api`;
 //   const localHost = "http://localhost:4004/api"
 
   useEffect(() => {
     async function fetchMembers() {
       setMembers(null);
-      const result = await axios.get(`api/members`);
+      const result = await axios.get(`/api/members`);
       if (!ignore) {
         setMembers(result);
       }
@@ -21,7 +21,7 @@ const Test = () => {
     return () => {
       ignore = true;
     };
-  }, [baseURL]);
+  }, []);
 
 
   if (members) {
