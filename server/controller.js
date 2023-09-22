47394536ef15e2636b5328c1f1d1854f7193720c
Caleb Hodges <chodgesdev@gmail.com>
2023-09-21 21:18:36 -0500

    test

--- server/controller.js
+++ server/controller.js
@@ -2,6 +2,7 @@ const db = require('./db.json')
 
 module.exports = {
     getMembers(req, res) {
+        console.log("sending", db)
         res.status(200).send(db)
     }
 }
--- server/index.js
+++ server/index.js
@@ -6,13 +6,15 @@ const controller = require('./controller')
 const port = 4004
 const app = express()
 
+console.log("1")
 app.use(cors())
 // app.use(express.static(`${__dirname}/public`))
-
+console.log("2")
 app.use(express.json())
 
-
+console.log("3")
 app.get('/api/members', controller.getMembers)
 
+console.log("4")
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
